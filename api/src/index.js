cb71c49d68c7e8187d5896611b197d71550f7782
Tiago Silva Cardoso - 06453943514 <tiago.s.cardoso@animaeducacao.com.br>
2023-10-08 12:21:53 -0300

    removing api

--- api/src/index.js
+++
@@ -1,5 +0,0 @@
-import startServer from './server';
-import typeDefs from './graphql/typeDefs';
-import resolvers from './graphql/resolvers';
-
-startServer({ typeDefs, resolvers });
--- api/src/server.js
+++
@@ -1,8 +0,0 @@
-import { ApolloServer } from 'apollo-server';
-
-function startServer({ typeDefs, resolvers }) {
-  const server = new ApolloServer({ typeDefs, resolvers });
-  server.listen().then(({ url }) => console.log(`🔥 Server started at ${url}`));
-}
-
-export default startServer;
